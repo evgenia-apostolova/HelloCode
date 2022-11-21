b53bc340999c08d5aab9a679c5683ef08a3c7196
Evgenia <evgenia.apostolova@gmail.com>
2022-11-22 02:07:58 +0300

    Example005 Condition If-Else added

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Add username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hey, this is MASHA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
