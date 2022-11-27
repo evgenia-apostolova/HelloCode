789cddad8e70b047514af231a7332d000939d64c
Evgenia <evgenia.apostolova@gmail.com>
2022-11-27 18:58:45 +0300

    Example011_ArrayLibrary edited

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,31 +1,30 @@
-﻿//void FillArray(int[] collection)
-//{
-//    int length = collection.Length;
-//    int index = 0;
-//    while (index < length)
-//    {
-//        collection[index] = new Random().Next(1, 10);
-//        index++;
-//    }
-//}
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
 
-
-//void PrintArray(int[] col)
-//{
-//    int count = col.Length;
-//   int position = 0;
-//    while (position < count)
-//    {
-//        Console.WriteLine(col[position]);
-//        position++;
-//  }
-//}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
 
 int IndexOf(int[] collection, int find)
 {
     int count = collection.Length;
     int index = 0;
-    int position = 0;
+    int position = -1;
 
     while (index < count)
     {
@@ -45,5 +44,5 @@ FillArray(array);
 PrintArray(array);
 Console.WriteLine();
 
-int pos = IndexOf(array, 4);
+int pos = IndexOf(array, 444);
 Console.WriteLine(pos);
