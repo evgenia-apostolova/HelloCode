6e96994e641aeab8863603b6998e76d5a3b5c0be
Evgenia <evgenia.apostolova@gmail.com>
2022-11-27 16:24:52 +0300

    Example012 folder edited

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,69 @@
+﻿// Type 1
+/*
+void Method1()
+{
+    Console.WriteLine("Author ...");
+}
+Method1();
+*/
+
+// Type 2
+/*
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Message text");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+    Console.WriteLine(msg);
+    i++;
+    }
+}
+Method21(msg: "Text", count: 4);
+*/
+// Type 3
+/*
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+// Type 4
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = "";
+    for ( int i = 0; i < count; i++)
+    while (int i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
+*/
+
+string Method4(int count, string text)
+{
+    string result = "";
+
+for (int i = 0; i <count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
+
